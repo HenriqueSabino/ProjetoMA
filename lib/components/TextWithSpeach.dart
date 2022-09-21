fe672d64b47873906077471f85765aa276845c64
sberdine77 <sbsx77xavier@hotmail.com>
2022-09-21 02:27:02 -0300

    Adding text to voice feature in all texts

--- lib/components/TextWithSpeach.dart
+++ lib/components/TextWithSpeach.dart
@@ -1,7 +1,10 @@
-import 'dart:ffi';
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class TextWithSpeach extends StatefulWidget {
   const TextWithSpeach({
@@ -14,7 +17,134 @@ class TextWithSpeach extends StatefulWidget {
   _TextWithSpeachState createState() => _TextWithSpeachState();
 }
 
+enum TtsState { playing, stopped, paused, continued }
+
 class _TextWithSpeachState extends State<TextWithSpeach> {
+  FlutterTts flutterTts;
+  String language;
+  String engine;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isPaused => ttsState == TtsState.paused;
+  get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWindows => !kIsWeb && Platform.isWindows;
+  bool get isWeb => kIsWeb;
+
+  @override
+  initState() {
+    super.initState();
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    if (isWeb || isIOS || isWindows) {
+      flutterTts.setPauseHandler(() {
+        setState(() {
+          print("Paused");
+          ttsState = TtsState.paused;
+        });
+      });
+
+      flutterTts.setContinueHandler(() {
+        setState(() {
+          print("Continued");
+          ttsState = TtsState.continued;
+        });
+      });
+    }
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  Future _speak(String text) async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+    await flutterTts.setLanguage("en-US");
+
+    if (text != null) {
+      if (text.isNotEmpty) {
+        setState(() => ttsState = TtsState.playing);
+        await flutterTts.speak(text);
+      }
+    }
+  }
+
+  Future _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  bool isPlaingVoice = false;
+
   List<InlineSpan> textSpansWithRecognizer(List<TextSpan> entryTextSpans) {
     List<InlineSpan> tempListTextSpans = [];
     if (entryTextSpans == null) {
@@ -27,7 +157,13 @@ class _TextWithSpeachState extends State<TextWithSpeach> {
         children: textSpansWithRecognizer(element.children),
         recognizer: LongPressGestureRecognizer()
           ..onLongPress = () {
-            print("Lonng press " + element.text);
+            print("Long press triggered");
+            if (isPlaying) {
+              print("Lonng press playing");
+              _stop();
+            } else {
+              _speak(element.text);
+            }
           },
         mouseCursor: element.mouseCursor,
         onEnter: element.onEnter,
